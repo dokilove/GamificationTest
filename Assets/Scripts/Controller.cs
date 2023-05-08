4725c2bd48f0dcfdaa4cc52c2a5c756500b72569
HyunjoonShim <hjshim@ipacom.co.kr>
2023-05-08 11:28:37 +0900

    Reflection Vector를 사용해서 stage rect밖으로 못나가게 해봄

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -44,62 +44,54 @@ public class Controller : MonoBehaviour
 
         while (velocity > 0.0f)
         {
+            // Rect 안에 있는지 체크합니다.
+            bool isInside = stage.layout.Contains(currentPos);
+
+            // Rect 안에 있지 않으면 방향을 바꿔줍니다.
+            if (!isInside)
+            {
+                // 현재 위치를 기준으로 Rect와 가장 가까운 점을 구합니다.
+                Vector2 closestPoint = ClosestPoint(stage.layout, currentPos);
+
+                // 가장 가까운 점과 현재 위치의 차이 벡터를 구합니다.
+                Vector2 difference = currentPos - closestPoint;
+
+                // 차이 벡터를 reflection vector로 변환합니다.
+                Vector2 reflection = Vector2.Reflect(difference, stage.layout.center - closestPoint).normalized;
+
+                // 새로운 방향을 reflection vector로 설정합니다.
+                direction = reflection;
+            }
+
             currentPos += direction * velocity * speed * Time.deltaTime;
+
+            Debug.Log(stage.layout.Contains(icon.layout.center));
 
             velocity -= friction;
 
-            if (currentPos.x < stage.layout.x || currentPos.x > stage.layout.width + stage.layout.x) 
-            {
-                direction.x = -direction.x;
-                AdjustPositionX(currentPos);
-            }
-            if (currentPos.y < stage.layout.y || currentPos.y > stage.layout.height + stage.layout.y)
-            {
-                direction.y = -direction.y;
-                AdjustPositionY(currentPos);
-            }
-
             icon.style.left = currentPos.x - icon.layout.width * 0.5f;
             icon.style.top = currentPos.y - icon.layout.height * 0.5f;
-
+      
             yield return null;
         }
     }
-
-    void AdjustPositionX(Vector2 currentPos)
+    public static Vector2 ClosestPoint(Rect rect, Vector2 point)
     {
-        float offset = 0;
-        if (currentPos.x < stage.layout.x)
+        Vector2 closestPoint = point;
+        closestPoint.x = Mathf.Clamp(closestPoint.x, rect.xMin, rect.xMax);
+        closestPoint.y = Mathf.Clamp(closestPoint.y, rect.yMin, rect.yMax);
+        if (!rect.Contains(point))
         {
-            offset = stage.layout.x - currentPos.x;
+            if (point.x < rect.xMin)
+                closestPoint.x = rect.xMin;
+            else if (point.x > rect.xMax)
+                closestPoint.x = rect.xMax;
+            if (point.y < rect.yMin)
+                closestPoint.y = rect.yMin;
+            else if (point.y > rect.yMax)
+                closestPoint.y = rect.yMax;
         }
-        else if (currentPos.x > stage.layout.width + stage.layout.x)
-        {
-            offset = stage.layout.width + stage.layout.x - currentPos.x;
-        }
-
-        if (Mathf.Abs(offset) > overlapThreshold)
-        {
-            currentPos.x += offset;
-        }
-    }
-
-    void AdjustPositionY(Vector2 currentPos)
-    {
-        float offset = 0;
-        if (currentPos.y < stage.layout.y)
-        {
-            offset = stage.layout.y - currentPos.y;
-        }
-        else if (currentPos.y > stage.layout.height + stage.layout.y)
-        {
-            offset = stage.layout.height + stage.layout.y - currentPos.y;
-        }
-
-        if (Mathf.Abs(offset) > overlapThreshold)
-        {
-            currentPos.y += offset;
-        }
+        return closestPoint;
     }
 
 }

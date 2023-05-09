b3ebcf38245805377e80169bd4f59bc1ecd9006f
HyunjoonShim <hjshim@ipacom.co.kr>
2023-05-09 13:42:55 +0900

    point 대신 원으로 충돌체크하게 로직 수정

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -47,6 +47,8 @@ public class Controller : MonoBehaviour
         float diffY = -diff.y;
         Vector2 direction = new Vector2(diffX, diffY).normalized;
 
+        float radius = icon.layout.width * 0.5f;
+
         while (velocity > 0.0f)
         {
             currentPos += direction * velocity * speed * Time.deltaTime;
@@ -60,18 +62,11 @@ public class Controller : MonoBehaviour
                 {
                     Rect obstacleRect = otherObjects[i].layout;
 
-                    if (PointRectCollision(currentPos, obstacleRect))
+                    if (RectCircleCollision(currentPos, radius, obstacleRect))
                     {
                         hasCollision = true;
 
-                        // Calculate the normal vector of the obstacle
-                        // Calculate the normal vector of the obstacle
-                        const float tolerance = 100.0f;
-                        Vector2 normal = new Vector2(
-                            Mathf.Abs(currentPos.x - obstacleRect.xMin) < tolerance ? -1.0f :
-                            Mathf.Abs(currentPos.x - obstacleRect.xMax) < tolerance ? 1.0f : 0.0f,
-                            Mathf.Abs(currentPos.y - obstacleRect.yMin) < tolerance ? -1.0f :
-                            Mathf.Abs(currentPos.y - obstacleRect.yMax) < tolerance ? 1.0f : 0.0f);
+                        Vector2 normal = GetCollisionNormal(currentPos, radius, obstacleRect);
 
                         // Calculate the reflection vector
                         Vector2 reflection = Vector2.Reflect(direction, normal);
@@ -111,7 +106,6 @@ public class Controller : MonoBehaviour
 
             yield return new WaitForFixedUpdate();
 
-            debugLabel.text = "start Velocity: " + startVelocity + " friction: " + friction +  "\nvelocity: " + velocity;
         }
     }
 
@@ -184,10 +178,10 @@ public class Controller : MonoBehaviour
 
     private Vector2 ClosestPointOnRectBoundary(Vector2 point, Rect rect)
     {
-        //if (rect.Contains(point))
-        //{
-        //    return point;
-        //}
+        if (rect.Contains(point))
+        {
+            return point;
+        }
 
         float closestX = Mathf.Clamp(point.x, rect.xMin, rect.xMax);
         float closestY = Mathf.Clamp(point.y, rect.yMin, rect.yMax);
@@ -203,10 +197,10 @@ public class Controller : MonoBehaviour
 
         return new Vector2(closestX, closestY);
     }
-    bool PointRectCollision(Vector2 point, Rect rect)
+    bool PointRectCollision(float radius, Vector2 point, Rect rect)
     {
-        if (point.x < rect.xMin || point.x > rect.xMax ||
-            point.y < rect.yMin || point.y > rect.yMax)
+        if (point.x + radius < rect.xMin || point.x - radius > rect.xMax ||
+            point.y + radius < rect.yMin || point.y - radius > rect.yMax)
         {
             // point is outside rect bounds
             return false;
@@ -214,5 +208,63 @@ public class Controller : MonoBehaviour
 
         return true;
     }
+
+    bool RectCircleCollision(Vector2 circleCenter, float circleRadius, Rect rect)
+    {
+        Vector2 closestPoint = new Vector2(
+            Mathf.Clamp(circleCenter.x, rect.xMin, rect.xMax),
+            Mathf.Clamp(circleCenter.y, rect.yMin, rect.yMax)
+        );
+
+        float distance = Vector2.Distance(circleCenter, closestPoint);
+
+        return distance < circleRadius;
+    }
+    public Vector2 GetCollisionNormal(Vector2 circlePos, float circleRadius, Rect rect)
+    {
+        Vector2 rectCenter = rect.center;
+        float rectWidth = rect.width;
+        float rectHeight = rect.height;
+
+        // calculate distance between circle center and rect center
+        float distX = Mathf.Abs(circlePos.x - rectCenter.x);
+        float distY = Mathf.Abs(circlePos.y - rectCenter.y);
+
+        // if the distance between the centers is less than half the width and half the height
+        // of the rect, then there is a collision
+        if (distX <= rectWidth / 2 && distY <= rectHeight / 2)
+        {
+            // calculate the normal vector based on the side of the rect that the circle collided with
+            float deltaX = circlePos.x - rectCenter.x;
+            float deltaY = circlePos.y - rectCenter.y;
+
+            float absDeltaX = Mathf.Abs(deltaX);
+            float absDeltaY = Mathf.Abs(deltaY);
+
+            float xSign = Mathf.Sign(deltaX);
+            float ySign = Mathf.Sign(deltaY);
+
+            Vector2 normal = Vector2.zero;
+
+            if (absDeltaX > absDeltaY)
+            {
+                normal.x = xSign;
+            }
+            else if (absDeltaX < absDeltaY)
+            {
+                normal.y = ySign;
+            }
+            else
+            {
+                normal.x = xSign;
+                normal.y = ySign;
+            }
+
+            return normal.normalized;
+        }
+
+        return Vector2.zero;
+    }
+
 
 }
